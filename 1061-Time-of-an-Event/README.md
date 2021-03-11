fc72f329870797351a9b69d5edf88f96c304e9e6
Rhama Krisner <66706928+rhama-krisner@users.noreply.github.com>
2021-03-10 23:34:26 -0300

    1061 Time of an Event

---
+++ 1061-Time-of-an-Event/README.md
@@ -0,0 +1,12 @@
+# 1061 Time of an Event
+
+This program calculates the difference between two dates and times, where the user must allocate the day, hour, minute and second of the beginning of an event, and the day, hour, minute and second of its end.
+
+The logic used was to follow the difference in days, with the hour, minute and second, convert to hours, remove the difference between all, and return with the due value for the user.
+___
+
+# 1061 Tempo de um Evento
+
+Este programa calcula a diferença entre duas datas e horas, onde o usuário deve imputar o dia, hora, minuto e segundo de inicio de um evento, e o dia, hora, minuto e segundo do seu término.
+
+A lógica utilizada foi, sonseguir a diferença dos dias, com a hora, minuto e segundo, converter em horas, tirar a diferençar entre todos, e retornar com o devido valor para o usuário.
---
+++ 1061-Time-of-an-Event/src/Main.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner tempo = new Scanner(System.in);
+
+        System.out.print("Dia ");
+        int dia1 = tempo.nextInt();
+        int hora1 = tempo.nextInt();
+        int minuto1 = tempo.nextInt();
+        int segundo1 = tempo.nextInt();
+
+        System.out.print("Dia ");
+        int dia2 = tempo.nextInt();
+        int hora2 = tempo.nextInt();
+        int minuto2 = tempo.nextInt();
+        int segundo2 = tempo.nextInt();
+
+        int hora = (hora1+hora2+((minuto1+minuto2)*60)+((segundo1+segundo2)*3600))+(dia1-dia2);
+
+        System.out.println((dia2-dia1)-(int) 24/(hora%24) + " dia (s)");
+        System.out.println(hora%24 + " hora (s)");
+        System.out.println(Math.abs(minuto1-minuto2) + " minuto (s)");
+        System.out.println(Math.abs(segundo1-segundo2) + " segundo (s)");
+    }
+}
