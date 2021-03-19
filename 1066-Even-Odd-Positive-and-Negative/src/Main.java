f93683c45671a164ecc8f1efa8410f07038dd78c
Rhama Krisner <66706928+rhama-krisner@users.noreply.github.com>
2021-03-18 21:37:16 -0300

    1066 corrigido

--- 1066-Even-Odd-Positive-and-Negative/src/Main.java
+++ 1066-Even-Odd-Positive-and-Negative/src/Main.java
@@ -1,15 +1,9 @@
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner ler = new Scanner(System.in);
-        List<Integer> listaPar = new ArrayList<>();
-        List<Integer> listaImpar = new ArrayList<>();
-        List<Integer> listaPositivo = new ArrayList<>();
-        List<Integer> listaNegativo = new ArrayList<>();
 
         int numero;
         int totalPositivo = 0;
@@ -21,21 +15,18 @@ public class Main {
             numero = ler.nextInt();
             if (numero > 0) {
                 totalPositivo += 1;
-                listaPositivo.add(numero);
             } else if (numero < 0) {
                 totalNegativo += 1;
-                listaNegativo.add(numero);
-            } else if (numero % 2 == 0) {
+            }
+            if (numero % 2 == 0) {
                 totalPar += 1;
-                listaPar.add(numero);
             } else if (numero % 2 != 0) {
                 totalImpar += 1;
-                listaImpar.add(numero);
             }
         }
-        System.out.println(listaPar.size() + " valor(es) par(es)");
-        System.out.println(listaImpar.size() + " valo(res) impar (es)");
-        System.out.println(listaPositivo.size() + " valo(res) positivo(s)");
-        System.out.println(listaNegativo.size() + " valor(es) negativo(os)");
+        System.out.println(totalPar + " valor(es) par(es)");
+        System.out.println(totalImpar + " valo(res) impar (es)");
+        System.out.println(totalPositivo + " valo(res) positivo(s)");
+        System.out.println(totalNegativo + " valor(es) negativo(os)");
     }
 }
